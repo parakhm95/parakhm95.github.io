8d556d751d836ad494ec7623525445e100893634
parakhm95 <parakhm95@gmail.com>
2025-02-04 21:45:00 +0000

    Deploying to gh-pages from @ parakhm95/parakhm95.github.io@a51e7bf9772b8fc4252be0325a0bf3fd60488802 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This page contains some of the highlights from my CV, and the rest can be found in the pdf attached.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
